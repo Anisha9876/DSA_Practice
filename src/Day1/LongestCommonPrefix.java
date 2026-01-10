33787d6373ea76a4e78c59ea33e69e3c85a47740
Anisha9876 <routrayanisha30@gmail.com>
2026-01-10 20:59:05 +0530

    Longest Common Prefix among all the strings given

--- src/Day1/LongestCommonPrefix.java
+++ src/Day1/LongestCommonPrefix.java
@@ -1,4 +1,27 @@
 package Day1;
 
 public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String strs[]={"flower","flow","flight"};
+        if(strs.length==0){
+
+            System.out.println("");
+            return;
+        }
+
+        String prefix=strs[0];
+
+        for(int i=0;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix=prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()){
+
+                    System.out.println("");
+                    return;
+                }
+            }
+
+        }
+        System.out.println(prefix);
+    }
 }

d32d157014c88e88c4b5bc7047cdeeb6674eea6b
Anisha9876 <routrayanisha30@gmail.com>
2026-01-27 23:00:20 +0530

    Remove Duplicates from the sorted array leetcode_24

---
+++ src/Array/Lt_24_RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lt_24_RemoveDuplicates {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,5,6,7,7};
+        int recent=arr[0];
+        List<Integer> a=new ArrayList<>();
+        a.add(arr[0]);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=recent){
+                a.add(arr[i]);
+            }
+            recent=arr[i];
+        }
+
+        System.out.println(a);
+    }
+}

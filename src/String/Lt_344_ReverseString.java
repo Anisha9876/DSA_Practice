46a66acab957f1158572061aa68a536d3e16cdca
Anisha9876 <routrayanisha30@gmail.com>
2026-01-28 20:31:27 +0530

    Reverse a String in optimal way

--- src/String/Lt_344_ReverseString.java
+++ src/String/Lt_344_ReverseString.java
@@ -2,11 +2,19 @@ package String;
 
 public class Lt_344_ReverseString {
     public static void main(String[] args) {
-        String str="anisha";
-        StringBuilder rev=new StringBuilder();
-        for(int i=str.length()-1;i>=0;i--){
-            rev.append(str.charAt(i));
+        char[] str={'a','n','i'};
+
+        char temp;
+        for(int i=0;i<str.length/2;i++)
+        {
+            temp=str[i];
+            str[i]=str[str.length-i-1];
+            str[str.length-i-1]=temp;
+
         }
-        System.out.println(rev);
+        for(char c:str){
+            System.out.print(c+" ");
+        }
+
     }
 }

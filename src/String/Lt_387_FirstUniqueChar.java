3aab460c272d2a8e4cc92518d0e2bc728e6c778e
Anisha9876 <routrayanisha30@gmail.com>
2026-02-03 11:00:11 +0530

    First Unique Character

---
+++ src/String/Lt_387_FirstUniqueChar.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.HashMap;
+
+public class Lt_387_FirstUniqueChar {
+    public static void main(String[] args) {
+        String str="Annarab";
+        char c;
+        HashMap<String ,Integer> map= new HashMap<>();
+
+
+    }
+}
